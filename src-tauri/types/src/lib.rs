516050919a2e833bcee4fc84604544423fdcdc08
Sahil Gupte <sahilsachingupte@gmail.com>
2024-08-29 21:42:58 -0400

    Gate types for extensions

--- src-tauri/types/src/lib.rs
+++ src-tauri/types/src/lib.rs
@@ -4,7 +4,11 @@ pub mod cache_schema;
 pub mod canvaz;
 pub mod common;
 pub mod entities;
+
+#[cfg(not(feature = "extensions"))]
 pub mod errors;
+
+#[cfg(not(feature = "extensions"))]
 pub mod mpris;
 pub mod preferences;
 #[cfg(feature = "core")]
@@ -12,11 +16,17 @@ pub mod schema;
 pub mod songs;
 
 pub mod providers;
+
+#[cfg(not(feature = "extensions"))]
 pub mod ui;
 
 #[cfg(feature = "core")]
 pub mod oauth;
 
 pub mod extensions;
+
+#[cfg(not(feature = "extensions"))]
 pub mod themes;
+
+#[cfg(not(feature = "extensions"))]
 pub mod window;

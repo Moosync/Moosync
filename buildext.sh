280a25bcf6406cdc67eb405c3462a98c297876ac
Sahil Gupte <sahilsachingupte@gmail.com>
2024-12-17 03:38:13 -0500

    Update build ext script

--- buildext.sh
+++ buildext.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd src-extensions-wasm
-rm -f ../src-tauri/binaries/exthost-wasm-*
+rm -rf ../src-tauri/binaries/exthost-wasm-*
 cargo build --release
+mkdir -p ../src-tauri/binaries
 cp target/release/wasm-extension-runner ../src-tauri/binaries/exthost-wasm-$(rustc -Vv | grep host | cut -f2 -d' ')

265284b5e6e2d0a5930999bea186d62f5f872d87
Sahil Gupte <sahilsachingupte@gmail.com>
2024-12-17 15:52:42 -0500

    Refactor directory structure

--- buildext.sh
+++ buildext.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cd src-extensions-wasm
-rm -rf ../src-tauri/binaries/exthost-wasm-*
+cd lib/extensions-wasm
+rm -rf ../../src-tauri/binaries/exthost-wasm-*
 cargo build --release
-mkdir -p ../src-tauri/binaries
-cp target/release/wasm-extension-runner ../src-tauri/binaries/exthost-wasm-$(rustc -Vv | grep host | cut -f2 -d' ')
+mkdir -p ../../src-tauri/binaries
+cp target/release/wasm-extension-runner ../../src-tauri/binaries/exthost-wasm-$(rustc -Vv | grep host | cut -f2 -d' ')

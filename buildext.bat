57a1c773234ee13d39807d0d147dab96abade023
Sahil Gupte <sahilsachingupte@gmail.com>
2025-01-18 17:18:38 -0500

    Remove build ext

--- buildext.bat
+++
@@ -1,9 +0,0 @@
-cd lib\extensions-wasm
-rmdir /s /q ..\..\src-tauri\binaries\
-cargo build --release
-mkdir ..\..\src-tauri\binaries
-for /f "tokens=2 delims=: " %%i in ('rustc -Vv ^| findstr host') do (
-    set "HOST=%%i"
-)
-copy target\release\wasm-extension-runner.exe ..\..\src-tauri\binaries\exthost-wasm-%HOST%.exe
-

db60ee8601a0e504ae3cc62764b95369d2d9203c
Sahil Gupte <sahilsachingupte@gmail.com>
2025-12-16 09:39:11 -0800

    Uncomment rustc-link-lib directive in build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -15,6 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 fn main() {
-    // println!("cargo:rustc-link-lib=z");
+    println!("cargo:rustc-link-lib=z");
     tauri_build::build()
 }

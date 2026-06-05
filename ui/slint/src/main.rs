160a1c6ecb5e3bbeda6c283cffac2545d8e5d17c
Sahil Gupte <sahilsachingupte@gmail.com>
2026-06-05 16:11:39 -0700

    Use jemalloc as default allocator

--- ui/slint/src/main.rs
+++ ui/slint/src/main.rs
@@ -1,5 +1,9 @@
 use slint_app::run;
 
+#[cfg(not(any(target_env = "msvc", target_os = "android")))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 #[cfg(not(target_os = "android"))]
 #[tokio::main]
 async fn main() { run().await; }

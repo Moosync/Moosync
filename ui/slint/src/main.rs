35fb97b7d7e95c2da65fedb6452cac3edddcc7e9
Sahil Gupte <sahilsachingupte@gmail.com>
2026-09-18 01:20:06 -0400

    linux: Disable hugepages
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- ui/slint/src/main.rs
+++ ui/slint/src/main.rs
@@ -1,12 +1,31 @@
+use std::thread::available_parallelism;
+
+#[cfg(target_os = "linux")]
+use prctl::set_thp_disable;
 use slint_app::run;
+use tokio::runtime::Builder;
 
 #[cfg(not(any(target_os = "windows", target_os = "android")))]
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 #[cfg(not(target_os = "android"))]
-#[tokio::main]
-async fn main() { run().await; }
+fn main() {
+    #[cfg(target_os = "linux")]
+    let _ = set_thp_disable(true);
+
+    let threads = available_parallelism()
+        .map(|parallelism| parallelism.get().clamp(2, 4))
+        .unwrap_or(4);
+
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(threads)
+        .enable_all()
+        .build()
+        .expect("Failed to build Tokio runtime");
+
+    runtime.block_on(run());
+}
 
 #[cfg(target_os = "android")]
 fn main() {}
